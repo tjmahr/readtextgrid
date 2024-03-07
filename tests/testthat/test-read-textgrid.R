57827a965b339f8662474f5920e09284bcfa490e
TJ Mahr <tjmahr@users.noreply.github.com>
2024-03-07 12:38:23 -0600

    encoding pull request (#9)
    * add example utf-16 textgrid
    
    * support encoding argument.
    
    closes #7. closes #8.
    
    ---------
    
    Co-authored-by: Jonathan Washington <jonathan.n.washington@gmail.com>

--- tests/testthat/test-read-textgrid.R
+++ tests/testthat/test-read-textgrid.R
@@ -21,3 +21,25 @@ test_that("example_textgrid works", {
   tg <- read_textgrid(path)
   expect_equal(nrow(tg), 3)
 })
+
+test_that("encoding support", {
+  example_textgrid(1) |>
+    read_textgrid(encoding = "UTF-8") |>
+    nrow() |>
+    expect_equal(3)
+
+  example_textgrid(1) |>
+    read_textgrid() |>
+    nrow() |>
+    expect_equal(3)
+
+  example_textgrid(2) |>
+    read_textgrid(encoding = "UTF-16") |>
+    nrow() |>
+    expect_equal(3)
+
+  example_textgrid(2) |>
+    read_textgrid() |>
+    nrow() |>
+    expect_equal(3)
+})
